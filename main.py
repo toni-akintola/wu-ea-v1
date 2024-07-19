4eb62611c243fec41b9c155c86fea98f665ad09b
toni-akintola <oakintol@nd.edu>
2024-07-19 10:47:07 -0400

    publishing initial draft of wu-ea-v1

---
+++ main.py
@@ -0,0 +1,110 @@
+from modelpy_abm.main import AgentModel
+import random
+import numpy as np
+
+# Template initial data and timestep data functions
+
+
+def generateInitialData():
+    proportion_marginalized = 1 / 6
+    return {
+        "a_success_rate": 0.5,
+        "b_success_rate": random.uniform(0.01, 0.99),
+        "b_evidence": None,
+        "type": "dominant"
+        if random.random() > proportion_marginalized
+        else "marginalized",
+    }
+
+
+def generateTimestepData(model: AgentModel):
+    graph = model.get_graph()
+    for _node, node_data in graph.nodes(data=True):
+        if node_data["a_success_rate"] > node_data["b_success_rate"]:
+            # agent won't have any new evidence gathered for b
+            node_data["b_evidence"] = None
+
+            # agent pulls the "b" bandit arm
+        else:
+            # agent collects evidence
+            node_data["b_evidence"] = int(
+                np.random.binomial(model["num_pulls"], model["objective_b"], size=None)
+            )
+        # define function to calculate posterior belief
+
+        def calculate_posterior(
+            prior_belief: float, num_evidence: float, devalue=False
+        ) -> float:
+            # Calculate likelihood, will be either the success rate
+            pEH_likelihood = (model["objective_b"] ** num_evidence) * (
+                (1 - model["objective_b"]) ** (model["num_pulls"] - num_evidence)
+            )
+
+            # Calculate normalization constant
+            if devalue:
+                pE_evidence = 1 - model["degree_devaluation"] * (1 - prior_belief)
+            else:
+                pE_evidence = (pEH_likelihood * prior_belief) + (
+                    (1 - model["objective_b"]) ** num_evidence
+                ) * (model["objective_b"] ** (model["num_pulls"] - num_evidence)) * (
+                    1 - prior_belief
+                )
+
+            # Calculate posterior belief using Bayes' theorem
+            posterior = (pEH_likelihood * prior_belief) / pE_evidence
+
+            return posterior
+
+        # update the beliefs, based on evidence and neighbors
+        for node, node_data in graph.nodes(data=True):
+            neighbors = graph.neighbors(node)
+            # update belief of "b" on own evidence gathered
+            if node_data["b_evidence"] is not None:
+                node_data["b_success_rate"] = calculate_posterior(
+                    node_data["b_success_rate"], node_data["b_evidence"]
+                )
+
+            # update node belief of "b" based on evidence gathered by neighbors
+            for neighbor_node in neighbors:
+                neighbor_evidence = graph.nodes[neighbor_node]["b_evidence"]
+                neighbor_type = graph.nodes[neighbor_node]["type"]
+
+                # update from all neighbors if current node is marginalized
+                if node_data["type"] == "marginalized" and neighbor_evidence:
+                    node_data["b_success_rate"] = calculate_posterior(
+                        node_data["b_success_rate"], neighbor_evidence
+                    )
+        model.set_graph(graph)
+
+
+# Define AgentModel instance
+model = AgentModel()
+
+# We can also define our parameters with this helper function
+model.update_parameters(
+    {
+        "num_nodes": 40,
+        "proportion_marginalized": 1 / 6,
+        "num_pulls": 1,
+        "objective_b": 0.51,
+        "p_ingroup": 0.7,
+        "p_outgroup": 0.3,
+        "degree_devaluation": 0.2,
+    }
+)
+
+
+model.set_initial_data_function(generateInitialData)
+model.set_timestep_function(generateTimestepData)
+
+# Initialize the graph
+model.initialize_graph()
+
+# Run for loop for number of timesteps
+timesteps = 100
+
+for _ in range(timesteps):
+    model.timestep()
+
+# Print results
+print(model.get_graph())
