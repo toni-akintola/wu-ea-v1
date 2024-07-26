0d9484e5bd7b4fba94720f6e4271f6be0d44bd20
toni-akintola <oakintol@nd.edu>
2024-07-26 12:31:34 -0400

    reconstructing wu-ea-v1

--- model.py
+++ model.py
@@ -1,126 +1,101 @@
-import statistics
-import networkx as nx
-import numpy as np
 import random
+import numpy as np
+from modelpy_abm.main import AgentModel
+
+# Template initial data and timestep data functions
 
 
-class EpistemicAdvantageModel:
-    def __init__(self):
-        # Define model type
-        # Define Parameters
-        self.num_agents = (
-            40  # number of nodes in network, alternates between 3, 6, 12, 18
-        )
-        # % population marginalized, alternates between 1/6, 1/3, 1/2, 2/3
-        self.proportion_marginalized = float(1 / 6)
-        self.num_pulls = 1  # number of arm pulls, alternates between 1, 5, 10, 20
-        # actual probability of B arm yielding success, alternates between .51, .55, .6, .7, .8
-        self.objective_b = 0.51
-        self.p_ingroup = 0.7
-        self.p_outgroup = 0.3
-        self.degree_devaluation = 0.2
+def constructModel() -> AgentModel:
+    def generateInitialData(model: AgentModel):
+        return {
+            "a_success_rate": 0.5,
+            "b_success_rate": random.uniform(0.01, 0.99),
+            "b_evidence": None,
+            "type": "dominant"
+            if random.random() > model.proportion_marginalized
+            else "marginalized",
+        }
 
-        # initialize the graph
-        self.graph: nx.Graph = None
-
-    def initialize_graph(self):
-        # Graph for variation1 will not be complete, edges will be added dynamically
-        self.graph = nx.Graph()
-        self.graph.add_nodes_from((x for x in range(1, self.num_agents)))
-
-        # Initialize all the nodes to this initial data
-
-        for node in self.graph.nodes():
-            initial_data = {
-                # bandit arm A is set to a 0.5 success rate in the decision process
-                "a_success_rate": 0.5,
-                # bandit arm B is a learned parameter for the agent. Initialize randomly
-                "b_success_rate": random.uniform(0.01, 0.99),
-                # agent evidence learned, will be used to update their belief and others in the network
-                "b_evidence": None,
-                # population type, 'marginalized' or 'dominant'
-                "type": "dominant"
-                if random.random() > self.proportion_marginalized
-                else "marginalized",
-            }
-
-            self.graph.nodes[node].update(initial_data)
-
-        for potential_neighbor in self.graph.nodes():
-            # each node is connected to itself
-            if potential_neighbor == node:
-                self.graph.add_edge(node, node)
-            # potential connection to an outgroup agent
-            elif (
-                self.graph.nodes[potential_neighbor]["type"]
-                != self.graph.nodes[node]["type"]
-            ):
-                if random.random() < self.p_outgroup:
-                    self.graph.add_edge(node, potential_neighbor)
-            # potential connection to an ingroup agent
-            else:
-                if random.random() < self.p_ingroup:
-                    self.graph.add_edge(node, potential_neighbor)
-
-        return self.graph
-
-    def timestep(self):
-        # run the experiments in all the nodes
-        for _node, node_data in self.graph.nodes(data=True):
-            # agent pulls the "a" bandit arm
+    def generateTimestepData(model: AgentModel):
+        graph = model.get_graph()
+        for _node, node_data in graph.nodes(data=True):
             if node_data["a_success_rate"] > node_data["b_success_rate"]:
                 # agent won't have any new evidence gathered for b
                 node_data["b_evidence"] = None
 
-            # agent pulls the "b" bandit arm
+                # agent pulls the "b" bandit arm
             else:
                 # agent collects evidence
                 node_data["b_evidence"] = int(
-                    np.random.binomial(self.num_pulls, self.objective_b, size=None)
+                    np.random.binomial(
+                        model["num_pulls"], model["objective_b"], size=None
+                    )
+                )
+            # define function to calculate posterior belief
+
+            def calculate_posterior(
+                prior_belief: float, num_evidence: float, devalue=False
+            ) -> float:
+                # Calculate likelihood, will be either the success rate
+                pEH_likelihood = (model["objective_b"] ** num_evidence) * (
+                    (1 - model["objective_b"]) ** (model["num_pulls"] - num_evidence)
                 )
 
-        # define function to calculate posterior belief
-        def calculate_posterior(
-            prior_belief: float, num_evidence: float, devalue=False
-        ) -> float:
-            # Calculate likelihood, will be either the success rate
-            pEH_likelihood = (self.objective_b**num_evidence) * (
-                (1 - self.objective_b) ** (self.num_pulls - num_evidence)
-            )
-
-            # Calculate normalization constant
-            if devalue:
-                pE_evidence = 1 - self.degree_devaluation * (1 - prior_belief)
-            else:
-                pE_evidence = (pEH_likelihood * prior_belief) + (
-                    (1 - self.objective_b) ** num_evidence
-                ) * (self.objective_b ** (self.num_pulls - num_evidence)) * (
-                    1 - prior_belief
-                )
-
-            # Calculate posterior belief using Bayes' theorem
-            posterior = (pEH_likelihood * prior_belief) / pE_evidence
-
-            return posterior
-
-        # update the beliefs, based on evidence and neighbors
-        for node, node_data in self.graph.nodes(data=True):
-            neighbors = self.graph.neighbors(node)
-            # update belief of "b" on own evidence gathered
-            if node_data["b_evidence"] is not None:
-                node_data["b_success_rate"] = calculate_posterior(
-                    node_data["b_success_rate"], node_data["b_evidence"]
-                )
-
-            # update node belief of "b" based on evidence gathered by neighbors
-            for neighbor_node in neighbors:
-                neighbor_evidence = self.graph.nodes[neighbor_node]["b_evidence"]
-                neighbor_type = self.graph.nodes[neighbor_node]["type"]
-
-                # update from all neighbors if current node is marginalized
-                if node_data["type"] == "marginalized" and neighbor_evidence:
-                    node_data["b_success_rate"] = calculate_posterior(
-                        node_data["b_success_rate"], neighbor_evidence
+                # Calculate normalization constant
+                if devalue:
+                    pE_evidence = 1 - model["degree_devaluation"] * (1 - prior_belief)
+                else:
+                    pE_evidence = (pEH_likelihood * prior_belief) + (
+                        (1 - model["objective_b"]) ** num_evidence
+                    ) * (
+                        model["objective_b"] ** (model["num_pulls"] - num_evidence)
+                    ) * (
+                        1 - prior_belief
                     )
 
-        return self.graph
+                # Calculate posterior belief using Bayes' theorem
+                posterior = (pEH_likelihood * prior_belief) / pE_evidence
+
+                return posterior
+
+            # update the beliefs, based on evidence and neighbors
+            for node, node_data in graph.nodes(data=True):
+                neighbors = graph.neighbors(node)
+                # update belief of "b" on own evidence gathered
+                if node_data["b_evidence"] is not None:
+                    node_data["b_success_rate"] = calculate_posterior(
+                        node_data["b_success_rate"], node_data["b_evidence"]
+                    )
+
+                # update node belief of "b" based on evidence gathered by neighbors
+                for neighbor_node in neighbors:
+                    neighbor_evidence = graph.nodes[neighbor_node]["b_evidence"]
+                    neighbor_type = graph.nodes[neighbor_node]["type"]
+
+                    # update from all neighbors if current node is marginalized
+                    if node_data["type"] == "marginalized" and neighbor_evidence:
+                        node_data["b_success_rate"] = calculate_posterior(
+                            node_data["b_success_rate"], neighbor_evidence
+                        )
+            model.set_graph(graph)
+
+    # Define AgentModel instance
+    model = AgentModel()
+
+    # We can also define our parameters with this helper function
+    model.update_parameters(
+        {
+            "num_nodes": 40,
+            "proportion_marginalized": float(1 / 6),
+            "num_pulls": 1,
+            "objective_b": 0.51,
+            "p_ingroup": 0.7,
+            "p_outgroup": 0.3,
+            "degree_devaluation": 0.2,
+        }
+    )
+
+    model.set_initial_data_function(generateInitialData)
+    model.set_timestep_function(generateTimestepData)
+
+    return model
