f4fce5020910f34544d1b956b344c10498d56a63
toni-akintola <oakintol@nd.edu>
2024-08-06 22:01:57 -0400

    fixing production issue

--- model.py
+++ model.py
@@ -3,80 +3,78 @@ import numpy as np
 from modelpy_abm.main import AgentModel
 
 
-def constructModel() -> AgentModel:
-    def generateInitialData(model: AgentModel):
-        return {
-            "a_success_rate": 0.5,
-            "b_success_rate": random.uniform(0.01, 0.99),
-            "b_evidence": None,
-            "type": "dominant"
-            if random.random() > model.proportion_marginalized
-            else "marginalized",
-        }
+def generateInitialData(model: AgentModel):
+    return {
+        "a_success_rate": 0.5,
+        "b_success_rate": random.uniform(0.01, 0.99),
+        "b_evidence": None,
+        "type": "dominant"
+        if random.random() > model.proportion_marginalized
+        else "marginalized",
+    }
 
-    def generateTimestepData(model: AgentModel):
-        graph = model.get_graph()
-        for _node, node_data in graph.nodes(data=True):
-            if node_data["a_success_rate"] > node_data["b_success_rate"]:
-                # agent won't have any new evidence gathered for b
-                node_data["b_evidence"] = None
 
-                # agent pulls the "b" bandit arm
+def generateTimestepData(model: AgentModel):
+    graph = model.get_graph()
+    for _node, node_data in graph.nodes(data=True):
+        if node_data["a_success_rate"] > node_data["b_success_rate"]:
+            # agent won't have any new evidence gathered for b
+            node_data["b_evidence"] = None
+
+            # agent pulls the "b" bandit arm
+        else:
+            # agent collects evidence
+            node_data["b_evidence"] = int(
+                np.random.binomial(model["num_pulls"], model["objective_b"], size=None)
+            )
+        # define function to calculate posterior belief
+
+        def calculate_posterior(
+            prior_belief: float, num_evidence: float, devalue=False
+        ) -> float:
+            # Calculate likelihood, will be either the success rate
+            pEH_likelihood = (model["objective_b"] ** num_evidence) * (
+                (1 - model["objective_b"]) ** (model["num_pulls"] - num_evidence)
+            )
+
+            # Calculate normalization constant
+            if devalue:
+                pE_evidence = 1 - model["degree_devaluation"] * (1 - prior_belief)
             else:
-                # agent collects evidence
-                node_data["b_evidence"] = int(
-                    np.random.binomial(
-                        model["num_pulls"], model["objective_b"], size=None
-                    )
-                )
-            # define function to calculate posterior belief
-
-            def calculate_posterior(
-                prior_belief: float, num_evidence: float, devalue=False
-            ) -> float:
-                # Calculate likelihood, will be either the success rate
-                pEH_likelihood = (model["objective_b"] ** num_evidence) * (
-                    (1 - model["objective_b"]) ** (model["num_pulls"] - num_evidence)
+                pE_evidence = (pEH_likelihood * prior_belief) + (
+                    (1 - model["objective_b"]) ** num_evidence
+                ) * (model["objective_b"] ** (model["num_pulls"] - num_evidence)) * (
+                    1 - prior_belief
                 )
 
-                # Calculate normalization constant
-                if devalue:
-                    pE_evidence = 1 - model["degree_devaluation"] * (1 - prior_belief)
-                else:
-                    pE_evidence = (pEH_likelihood * prior_belief) + (
-                        (1 - model["objective_b"]) ** num_evidence
-                    ) * (
-                        model["objective_b"] ** (model["num_pulls"] - num_evidence)
-                    ) * (
-                        1 - prior_belief
-                    )
+            # Calculate posterior belief using Bayes' theorem
+            posterior = (pEH_likelihood * prior_belief) / pE_evidence
 
-                # Calculate posterior belief using Bayes' theorem
-                posterior = (pEH_likelihood * prior_belief) / pE_evidence
+            return posterior
 
-                return posterior
+        # update the beliefs, based on evidence and neighbors
+        for node, node_data in graph.nodes(data=True):
+            neighbors = graph.neighbors(node)
+            # update belief of "b" on own evidence gathered
+            if node_data["b_evidence"] is not None:
+                node_data["b_success_rate"] = calculate_posterior(
+                    node_data["b_success_rate"], node_data["b_evidence"]
+                )
 
-            # update the beliefs, based on evidence and neighbors
-            for node, node_data in graph.nodes(data=True):
-                neighbors = graph.neighbors(node)
-                # update belief of "b" on own evidence gathered
-                if node_data["b_evidence"] is not None:
+            # update node belief of "b" based on evidence gathered by neighbors
+            for neighbor_node in neighbors:
+                neighbor_evidence = graph.nodes[neighbor_node]["b_evidence"]
+                neighbor_type = graph.nodes[neighbor_node]["type"]
+
+                # update from all neighbors if current node is marginalized
+                if node_data["type"] == "marginalized" and neighbor_evidence:
                     node_data["b_success_rate"] = calculate_posterior(
-                        node_data["b_success_rate"], node_data["b_evidence"]
+                        node_data["b_success_rate"], neighbor_evidence
                     )
+        model.set_graph(graph)
 
-                # update node belief of "b" based on evidence gathered by neighbors
-                for neighbor_node in neighbors:
-                    neighbor_evidence = graph.nodes[neighbor_node]["b_evidence"]
-                    neighbor_type = graph.nodes[neighbor_node]["type"]
 
-                    # update from all neighbors if current node is marginalized
-                    if node_data["type"] == "marginalized" and neighbor_evidence:
-                        node_data["b_success_rate"] = calculate_posterior(
-                            node_data["b_success_rate"], neighbor_evidence
-                        )
-            model.set_graph(graph)
-
+def constructModel() -> AgentModel:
     # Define AgentModel instance
     model = AgentModel()
 
